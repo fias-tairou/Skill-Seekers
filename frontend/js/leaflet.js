d885566754218f7632f7f1cd5485a31dbd3ba386
mohamed <mohamed.errahil2@gmail.com>
2024-03-04 19:14:58 +0000

    mijn deel branche blacklisted

--- frontend/js/leaflet.js
+++ frontend/js/leaflet.js
@@ -1,9 +1,11 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.22992, 4.41612], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+
+
+
+L.marker([51.22992, 4.41612]).addTo(map);
