7b8b35469912b1f296a3149152d281aeceb46196
Malik Gounachev <s149012@ap.be>
2024-03-17 16:16:23 +0000

    login page changes

--- frontend/scripts/landing-page.js
+++
@@ -1,9 +0,0 @@
-function projectNotAvailable(){
-    alert("Dit project is niet beschikbaar.")
-}
-
-const projectElements = document.querySelectorAll('#otherProject');
-
-projectElements.forEach(function(e) {
-    e.addEventListener('click', projectNotAvailable);
-});
